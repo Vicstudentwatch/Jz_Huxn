bb4db1d61f976c6b4430ebf9c26b50a5a9c90214
Victor Muhita <wangombevictor5@gmail.com>
2025-06-05 18:56:13 +0300

    do exercise 'style'

--- 7. Style/exercise/app.js
+++ 7. Style/exercise/app.js
@@ -1,11 +1,19 @@
 const container = document.querySelector(".container");
-const h1 = document.querySelector(".main-headings");
+const h2 = document.querySelector(".main-headings");
 const p = document.querySelector(".sub-headings");
 const btn = document.querySelector(".btn");
 
+const body = document.querySelector("body")
+body.style.margin = "0px";
 // 1. Proivde "400px" of height ot container element
+container.style.height = "400px";
 // 2. Change the background color of container to "teal"
-// 3. Change the color of h1 to "skyblue"
+container.style.backgroundColor = "teal";
+// 3. Change the color of h2 to "skyblue"
+h2.style.color = "skyblue"
 // 4. Change the fontFamily of paragraph to "sans-serif"
+p.style.fontFamily = "sans-serif";
 // 5. Change the color of paragraph to "white"
+p.style.color = "white";
 // 6. Change the color of btn to pink
+btn.style.color = "pink";
